2aa864276d7a2f809ed2258c6b845a70a57c4e01
SakshamL <sakshamluthra2016@gmail.com>
2024-07-31 01:11:14 +0530

    movies details page creaed and improved with data and poster added

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Header from "../modules/header";
+import "../pages/Details.css";
 
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 
@@ -26,13 +27,33 @@ const Details = () => {
   return (
     <>
       <Header />
-      <div className="content_card">
-        <img
-          className="posters-img"
-          src={IMGPATH + movie.poster_path}
-          alt="poster-img"
-        />
-        <span className="content_title">{movie.title}</span>
+      <div className="details-container">
+        <div className="backdrop-container">
+          <img
+            className="backdrop-img"
+            src={IMGPATH + movie.backdrop_path}
+            alt="banner"
+          />
+        </div>
+        <div className="details-body-container">
+          <div className="poster-img-container">
+            <img
+              className="details-poster-img"
+              src={IMGPATH + movie.poster_path}
+              alt="poster-img"
+            />
+            <div className="details-container">
+              <span className="movie-title">{movie.title}</span>
+              <p>Release Date: {movie.release_date}</p>
+              <br></br>
+              <p>Overview: {movie.overview}</p>
+            </div>
+          </div>
+        </div>
+        {/* <br></br>
+        <br></br>
+        <br></br> */}
+        <p style={{ color: "white" }}>jhasfbjsbj</p>
       </div>
     </>
   );
