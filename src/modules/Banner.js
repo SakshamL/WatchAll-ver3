ec756a0ffa0275ace23bcdb6de2d78611044a0bb
SakshamL <sakshamluthra2016@gmail.com>
2024-08-02 00:42:24 +0530

    Movies tab in header designed movie list

--- src/modules/Banner.js
+++ src/modules/Banner.js
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Banner.css";
 // import { Routes, Route } from "react-router-dom";
 
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+
 function Banner() {
+  const [popularMovies, setPopularMovies] = useState([]);
+
+  useEffect(() => {
+    getPopularMovies();
+  }, []);
+
+  const getPopularMovies = async () => {
+    const response = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=04c35731a5ee918f014970082a0088b1"
+    );
+    const responseJSON = await response.json();
+
+    // console.log(responseJSON.results);
+    setPopularMovies(responseJSON.results);
+  };
+
   return (
     <div>
       <div className="banner-container">
-        <img
-          className="banner-img"
-          src="./images/you_banner.jpg"
-          alt="banner-1"
-        />
-        <img
+        {popularMovies.slice(0, 4).map((movie) => {
+          return (
+            <React.Fragment key={movie.id}>
+              <img
+                className="banner-img"
+                src={IMGPATH + movie.backdrop_path}
+                alt="banner"
+              />
+            </React.Fragment>
+          );
+        })}
+
+        {/* <img
           className="banner-img"
           src="./images/heroes_banner.jpg"
           alt="banner-2"
@@ -25,7 +50,7 @@ function Banner() {
           className="banner-img"
           src="./images/you_banner.jpg"
           alt="banner-1"
-        />
+        /> */}
       </div>
     </div>
   );
--- src/pages/Movies_page.js
+++ src/pages/Movies_page.js
@@ -1,11 +1,63 @@
 import React from "react";
 import Header from "../modules/header";
+import "./Moviespg.css";
+import { useState, useEffect } from "react";
+import movies_api from "../APIURL";
+import { Link } from "react-router-dom";
+
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 
 function Moviespage() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  const getMovies = async () => {
+    const response = await fetch(movies_api.movies);
+    const responseJSON = await response.json();
+
+    setMovies(responseJSON.results);
+    // console.log(responseJSON);
+  };
+
   return (
     <>
       <Header />
-      <div>Movies</div>
+      <div className="movies-pg-container">
+        <div className="movies-cat-sidebar">
+          <p>SideBar</p>
+          <p>SideBar</p>
+          <p>SideBar</p>
+        </div>
+        <div className="movies-pg-list">
+          <div className="posters-row">
+            <div className="poster-container">
+              {movies.map((item) => {
+                if (item.poster_path !== null) {
+                  return (
+                    <React.Fragment key={item.id}>
+                      <Link to={`/movie/${item.id}`}>
+                        <div className="content_card">
+                          <img
+                            className="posters-img"
+                            src={IMGPATH + item.poster_path}
+                            alt="poster-img"
+                          />
+                          <span className="content_title">{item.title}</span>
+                        </div>
+                      </Link>
+                    </React.Fragment>
+                  );
+                } else {
+                  return null;
+                }
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
